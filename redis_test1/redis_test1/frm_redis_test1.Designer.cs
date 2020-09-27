080e41a8ce0a554efc335e98fd78fb883f2291b0
pc zeng <zengpc@hotmail.com>
2020-09-27 17:43:29 +0800

    up load

--- redis_test1/redis_test1/frm_redis_test1.Designer.cs
+++ redis_test1/redis_test1/frm_redis_test1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // frm_redis_test1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Name = "frm_redis_test1";
             this.Text = "Redis-Test1";
+            this.Load += new System.EventHandler(this.frm_redis_test1_Load);
             this.ResumeLayout(false);
 
         }
--- redis_test1/redis_test1/frm_redis_test1.cs
+++ redis_test1/redis_test1/frm_redis_test1.cs
@@ -16,5 +16,17 @@ namespace redis_test1
         {
             InitializeComponent();
         }
+
+        private void frm_redis_test1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace + "\r\n" + ex.Message);
+            }
+        }
     }
 }
